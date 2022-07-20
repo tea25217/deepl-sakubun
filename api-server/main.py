725c440cbbca0cb58b3171a223046375b468fb21
yrberrytea <yrberrytea@gmail.com>
2022-07-21 02:17:00 +0900

    レスポンスに結果コードを追加

--- api-server/main.py
+++ api-server/main.py
@@ -29,17 +29,19 @@ def translate(params: Params):
     translator = prepareTranslator(params)
 
     if not all([translator, params.text, params.target_lang]):
-        return {"message": "Required parameters are not completed."}
+        return {"result": "NG",
+                "message": "Required parameters are not completed."}
 
     try:
         result = translator.translate_text(
             params.text, target_lang=params.target_lang)
     except exceptions.AuthorizationException:
-        return {"message": "API key is wrong."}
+        return {"result": "NG", "message": "API key is wrong."}
     except exceptions.DeepLException:
-        return {"message": "API access is failed."}
+        return {"result": "NG", "message": "API access is failed."}
 
-    return {"message": "Successfully translated.",
+    return {"result": "OK",
+            "message": "Successfully translated.",
             "detected_source_lang": result.detected_source_lang,
             "text": result.text}
 
@@ -51,15 +53,16 @@ def usage(params: Params):
     try:
         usage = translator.get_usage()
     except exceptions.AuthorizationException:
-        return {"message": "API key is wrong."}
+        return {"result": "NG", "message": "API key is wrong."}
     except exceptions.DeepLException:
-        return {"message": "API access is failed."}
+        return {"result": "NG", "message": "API access is failed."}
     except AttributeError:
-        return {"message": "API key is required."}
+        return {"result": "NG", "message": "API key is required."}
 
     if usage.character.valid:
-        return {"message": "Character usage is checked.",
+        return {"result": "OK",
+                "message": "Character usage is checked.",
                 "count": usage.character.count,
                 "limit": usage.character.limit}
 
-    return {"message": "Something is wrong."}
+    return {"result": "NG", "message": "Something is wrong."}
